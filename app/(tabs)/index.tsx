da02562a56a84c13d5a15033f646727fdd989c25
Shehan Wickramasooriya <shehanwickramasooriya05@gmail.com>
2024-10-08 07:45:39 +0530

    Add question page 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
+import Question2 from "@/components/Question2";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Tab</Text>
+      {/* <Text style={styles.title}>Home Tab</Text> */}
+      <Question2 />
     </View>
   );
 }
