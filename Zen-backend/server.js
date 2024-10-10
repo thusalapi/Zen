0e5da0d86f233478af97e23a3542092013389d7d
SAwandya <sachilaawandya@gmail.com>
2024-10-10 19:05:13 +0530

    add backend

---
+++ Zen-backend/server.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const habits = require("./routes/habitRoutes");
+const users = require("./routes/userRoutes");
+
+mongoose
+  .connect(
+    "mongodb+srv://sachilaawandya:sachila20000816@habit-cluster.cldoa.mongodb.net/?retryWrites=true&w=majority&appName=habit-cluster"
+  )
+  .then(() => console.log("Connect to MongoDB"))
+  .catch((err) => console.log("Could not connect to MongoDB", err));
+
+app.use(cors()); // Enable CORS for all routes
+
+app.use(express.json());
+
+app.use("/api/habits", habits);
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
